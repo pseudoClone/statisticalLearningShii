e3a554de68d73f90a4cd75277d9bdaa18970d7b5
pseudoClone <prince.077bei027@acem.edu.np>
2025-06-16 15:19:49 +0545

    test(py): Test enumerate class on list of dictionary

---
+++ shitCode/py/enumerate5.py
@@ -0,0 +1,8 @@
+people = [
+    {"name": "Alice", "age": 25},
+    {"name": "Bob", "age": 30},
+    {"name": "Charlie", "age": 35}
+]
+
+for person in people:
+    print(f"{person['name']} is {person['age']} years old")
